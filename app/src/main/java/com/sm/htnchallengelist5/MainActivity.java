3ed7cf31ab63f06194bb46c2f389edfb61e286a5
marcokstephen <marcok.stephen@gmail.com>
2015-02-17 16:05:57 -0500

    removing options menu

--- app/src/main/java/com/sm/htnchallengelist5/MainActivity.java
+++ app/src/main/java/com/sm/htnchallengelist5/MainActivity.java
@@ -1,13 +1,9 @@
 package com.sm.htnchallengelist5;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,20 +31,5 @@ public class MainActivity extends Activity {
     public static void populateRecycler(){
         recycleViewAdapter = new RecycleViewAdapter(attendees);
         recyclerView.setAdapter(recycleViewAdapter);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
